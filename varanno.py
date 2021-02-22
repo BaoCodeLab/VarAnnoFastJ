8adaba6a96d7573c33c18ba763049fce64c9caad
ybao2 <57085049+ybao2@users.noreply.github.com>
2021-02-22 21:21:34 +0800

    Update varanno.py

--- varanno.py
+++ varanno.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ### Command-line interface for VarAnnoFastJ
 
-from annolib import parameters2
+from annolib import parameters3
 
 if __name__ == "__main__":
-    args=parameters2.parse_arguments()
+    args=parameters3.parse_arguments()
     args.func(args)
 
 
